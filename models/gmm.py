be15d5ed35c574f85f5e7ee993b522f372ce330e
Tannaz Monajemi <tannaz.monajemi@gmail.com>
2023-12-06 18:32:03 +0000

    fit Gaussian Mixed Model(GMM) and alalyse the results

---
+++ models/gmm.py
@@ -0,0 +1,11 @@
+# Z-score tells how many standard deviations away a given observation is from
+# the mean. For example, a Z score of 2.5 means that the data point is 2.5 standard
+# deviation far from the mean. And since it is far from the center, it’s flagged as
+# an outlier/anomaly. The Z-score measures how far a data point is away from the
+# mean as a signed multiple of the standard deviation. Large absolute values of
+# the Z-score suggest an anomaly.
+
+#  GMM : A Gaussian mixture model is a probabilistic model that assumes all the data
+#  points are generated from a mixture of a finite number of Gaussian distributions with
+#  unknown parameters.
+
